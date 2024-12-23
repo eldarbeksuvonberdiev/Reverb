7850e12e5258b6445f6dd6b90527f1f8298cac5c
eldarbeksuvonberdiev <eldorbek.suvonberdiev@mail.ru>
2024-12-23 20:22:11 +0500

    Breeze o'rnatdim

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,25 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ReadMessageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 
 Route::get('/message',[MessageController::class,'index'])->name('message.index');
